2dfdfcfeca4339a47e57d260d2d990166bc1a85f
ivi0540 <ivi0540@gmail.com>
2023-09-20 12:58:52 +0300

    feat: working on an audio player

--- audio-player/script.js
+++ audio-player/script.js
@@ -1,11 +1,24 @@
-let audio = document.querySelector('audio');
-let playBtn = document.querySelector('.playBtn');
-let pauseBtn = document.querySelector('.pauseBtn');
+// let audio = document.querySelector('audio');
+let audio = new Audio();
+// let playBtn = document.querySelector('.playBtn');
+// let pauseBtn = document.querySelector('.pauseBtn');
+let playOrPauseBtn = document.querySelector('.playOrPauseBtn');
+let isPlay = false;
 
-playBtn.addEventListener('click', () => {
-    audio.currentTime = 0;
-    audio.play();
-});
-pauseBtn.addEventListener('click', () => {
-    audio.pause();
+playOrPauseBtn.addEventListener('click', () => {
+    if (!isPlay) {
+        audio.src = "music/Gaga - Poker Face.mp3";
+        audio.currentTime = 0;
+        audio.play();
+
+        isPlay = true;
+        playOrPauseBtn.classList.toggle('pause');
+    } else {
+        audio.pause();
+
+        isPlay = false;
+
+        playOrPauseBtn.classList.toggle('pause');
+
+    };
 });
