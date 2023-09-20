64d1a6d76ca0d609f2f60fc8a9d47247bcc0824a
ivi0540 <ivi0540@gmail.com>
2023-09-20 22:57:27 +0300

    feat: added main layout player

--- audio-player/script.js
+++ audio-player/script.js
@@ -1,24 +1,27 @@
-// let audio = document.querySelector('audio');
 let audio = new Audio();
-// let playBtn = document.querySelector('.playBtn');
-// let pauseBtn = document.querySelector('.pauseBtn');
 let playOrPauseBtn = document.querySelector('.playOrPauseBtn');
 let isPlay = false;
 
-playOrPauseBtn.addEventListener('click', () => {
+let trackList = [
+    {
+        'singer': 'Lady Gaga',
+        'trackName': 'Poker Face',
+        'trackSrc': "music/Gaga - Poker Face.mp3",
+    }
+];
+
+function playOrPause() {
     if (!isPlay) {
         audio.src = "music/Gaga - Poker Face.mp3";
         audio.currentTime = 0;
         audio.play();
-
         isPlay = true;
         playOrPauseBtn.classList.toggle('pause');
     } else {
         audio.pause();
-
         isPlay = false;
-
         playOrPauseBtn.classList.toggle('pause');
-
     };
-});
+};
+
+playOrPauseBtn.addEventListener('click', playOrPause);
