2c8b50dad16458198452a005318d4ca90634dcd3
ivi0540 <ivi0540@gmail.com>
2023-09-26 22:44:14 +0300

    feat: added images

--- image-galery/script.js
+++ image-galery/script.js
@@ -1,8 +1,22 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const url = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+let searchBtn = document.querySelector('.searchBtn');
+let content = document.querySelector('.content');
+let arrData = [];
 
 async function getData() {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    for (img of data.results) {
+        arrData.push(img.urls.regular);
+    };
 }
-// getData();
+getData();
+
+searchBtn.addEventListener('click', () => {
+    for (elem of arrData) {
+        let img = document.createElement('img');
+        img.className = 'img';
+        img.src = elem;
+        content.append(img);
+    };
+});
