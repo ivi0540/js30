0575f72a3eb2c7931128359f2aaab6d3bde8f317
ivi0540 <ivi0540@gmail.com>
2023-09-27 12:31:04 +0300

    feat: added images save2

--- image-galery/script.js
+++ image-galery/script.js
@@ -1,18 +1,20 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 let searchBtn = document.querySelector('.searchBtn');
 let content = document.querySelector('.content');
+let searchField = document.querySelector('.searchField');
 let arrData = [];
 
-async function getData() {
+async function getData(url) {
     const res = await fetch(url);
     const data = await res.json();
     for (img of data.results) {
         arrData.push(img.urls.regular);
     };
 }
-getData();
 
 searchBtn.addEventListener('click', () => {
+    let query1 = String(searchField.value);
+    let url2 = 'https://api.unsplash.com/search/photos?query=' + query1 + '&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+    getData(url2);
     for (elem of arrData) {
         let img = document.createElement('img');
         img.className = 'img';
