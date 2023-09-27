9f658d44ecada8b896bfa2ce561a7e07aea8fcb9
ivi0540 <ivi0540@gmail.com>
2023-09-27 22:59:31 +0300

    feat: added image update function

--- image-galery/script.js
+++ image-galery/script.js
@@ -1,24 +1,37 @@
 let searchBtn = document.querySelector('.searchBtn');
 let content = document.querySelector('.content');
 let searchField = document.querySelector('.searchField');
-let arrData = [];
 
+
+
+
+let arrData = [];
 async function getData(url) {
     const res = await fetch(url);
     const data = await res.json();
+
+    arrData = [];
     for (img of data.results) {
         arrData.push(img.urls.regular);
     };
-}
-
-searchBtn.addEventListener('click', () => {
-    let query1 = String(searchField.value);
-    let url2 = 'https://api.unsplash.com/search/photos?query=' + query1 + '&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
-    getData(url2);
     for (elem of arrData) {
         let img = document.createElement('img');
         img.className = 'img';
         img.src = elem;
         content.append(img);
     };
+}
+
+function deleteImage() {
+    let elems = document.querySelectorAll('.img');
+    for (elem of elems) {
+        elem.remove();
+    };
+}
+
+searchBtn.addEventListener('click', () => {
+    deleteImage();
+    let query1 = String(searchField.value);
+    let url = 'https://api.unsplash.com/search/photos?query=' + query1 + '&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+    getData(url);
 });
