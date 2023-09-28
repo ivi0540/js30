4976144c3fb09871751bcafae529901514973498
ivi0540 <ivi0540@gmail.com>
2023-09-28 22:40:29 +0300

    feat: added footer

--- image-galery/script.js
+++ image-galery/script.js
@@ -29,9 +29,28 @@ function deleteImage() {
     };
 }
 
-searchBtn.addEventListener('click', () => {
+function init(q) {
+    deleteImage();
+    let query1 = String(q);
+    // let url = 'https://api.unsplash.com/search/photos?query=' + query1 + '&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+    let url = 'https://api.unsplash.com/search/photos?query=' + query1 + '&client_id=q52S8tuFb3wGFeOOI0f5zOcZN6ahg5309J3QyE6pUWg';
+    getData(url);
+};
+init("art");
+
+function Go() {
     deleteImage();
     let query1 = String(searchField.value);
-    let url = 'https://api.unsplash.com/search/photos?query=' + query1 + '&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+    let url = 'https://api.unsplash.com/search/photos?query=' + query1 + '&client_id=q52S8tuFb3wGFeOOI0f5zOcZN6ahg5309J3QyE6pUWg';
     getData(url);
+};
+
+searchBtn.addEventListener('click', () => {
+    Go();
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.code === 'Enter') {
+        Go();
+    };
 });
