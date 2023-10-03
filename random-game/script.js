5741bc49b9647a26a17f6121ac4a385da9eee210
ivi0540 <ivi0540@gmail.com>
2023-10-03 23:11:30 +0300

    feat: added pictures for floppy bird

--- random-game/script.js
+++ random-game/script.js
@@ -1,1 +1,28 @@
-// alert(228);
+var cvs = document.getElementById('canvas');
+var ctx = cvs.getContext('2d');
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src = 'images/bird.png';
+bg.src = 'images/bg.png';
+fg.src = 'images/fg.png';
+pipeUp.src = 'images/pipeUp.png';
+pipeBottom.src = 'images/pipeBottom.png';
+
+let gap = 90;//Зазор между препятствеями
+
+function draw() {
+    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(pipeUp, 100, 0);
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, 10, 50);
+};
+
+pipeBottom.onload = draw;
+
+
