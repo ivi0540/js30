f2340bd154a9b700b3627f2e362e83d729ede0f9
ivi0540 <ivi0540@gmail.com>
2023-10-08 13:25:40 +0300

    feat: added table score

--- random-game/script.js
+++ random-game/script.js
@@ -1,11 +1,24 @@
-var cvs = document.getElementById('canvas');
-var ctx = cvs.getContext('2d');
+let scoreDiv = document.querySelector('.score');
+let mainPanel = document.querySelector('.mainPanel');
+let startGame = document.querySelector('.startGame');
+let saveScore = document.querySelector('.saveScore');
 
-var bird = new Image();
-var bg = new Image();
-var fg = new Image();
-var pipeUp = new Image();
-var pipeBottom = new Image();
+let playGamePanel = document.querySelector('.playGamePanel');
+let csorePanel = document.querySelector('.csorePanel');
+let restartBtn = document.querySelector('#restartBtn');
+let scoreBall = document.querySelector('.scoreBall');
+let userName = document.querySelector('.userName');
+let tableScore = document.querySelector('.tableScore');
+let blockWithResults = document.querySelector('.blockWithResults');
+
+let cvs = document.getElementById('canvas');
+let ctx = cvs.getContext('2d');
+
+let bird = new Image();
+let bg = new Image();
+let fg = new Image();
+let pipeUp = new Image();
+let pipeBottom = new Image();
 
 bird.src = 'images/bird.png';
 bg.src = 'images/bg.png';
@@ -13,16 +26,201 @@ fg.src = 'images/fg.png';
 pipeUp.src = 'images/pipeUp.png';
 pipeBottom.src = 'images/pipeBottom.png';
 
-let gap = 90;//Зазор между препятствеями
+//Зазор между препятствеями
+let gap = 110;
+
+//Позиция птички
+let xPos = 10;
+let yPos = 150;
+
+//Гравитация
+let grav = 0.5;
+
+//Интервал появления блоков
+let blockInt = 90;
+
+//Рисование блоков
+let pipe = [];
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+};
+
+//Счет
+let score = 0;
+
+//Идет игра или нет
+let playGame = true;
 
 function draw() {
     ctx.drawImage(bg, 0, 0);
-    ctx.drawImage(pipeUp, 100, 0);
-    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+
+    for (let i in pipe) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+        //Скорость движения блоков
+        pipe[i].x = pipe[i].x - 0.5;
+
+        if (pipe[i].x === blockInt) {
+            pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        };
+
+        if (
+            (xPos + bird.width >= pipe[i].x) &&
+            (xPos <= pipe[i].x + pipeUp.width) &&
+            (
+                (yPos <= pipe[i].y + pipeUp.height) ||
+                (yPos + bird.height >= pipe[i].y + pipeUp.height + gap)
+            )
+        ) {
+            // cancelAnimationFrame();
+
+            closeCanvas();
+            showMainPanel();
+            onCsorePanel();
+            playGame = false;
+            // addScoreTable(JSON.parse(localStorage.getItem('totalScore')));
+        };
+
+        if ((pipe[i].x === 5) && (playGame === true)) {
+            score += 1;
+            scoreDiv.textContent = 'Score : ' + String(score);
+            scoreBall.textContent = 'Score : ' + String(score);
+        };
+    };
+
+
     ctx.drawImage(fg, 0, cvs.height - fg.height);
-    ctx.drawImage(bird, 10, 50);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+    requestAnimationFrame(draw);
 };
 
-pipeBottom.onload = draw;
+//При нажатии на любую кнопку птичка будет подпрыгивать вверх
+document.addEventListener('keydown', () => {
+    yPos -= 20;
+});
+
+//Необходимо подождать пока все картинки загрузятся а потом вызвать drow для рисования
+startGame.addEventListener('click', () => {
+    closeMainPanel();
+    showCanvas();
+    pipeBottom.onload = draw;
+    requestAnimationFrame(draw);
+});
+
+saveScore.addEventListener('click', () => {
+    let player = {
+        'userName': userName.value,
+        'score': scoreBall.textContent
+    };
+
+    let n = JSON.parse(localStorage.getItem('totalScore'));
+    if (n === null) {
+        n = [];
+    };
+    n.push(player);
+    localStorage.setItem('totalScore', JSON.stringify(n));
+
+    addScoreTable(JSON.parse(localStorage.getItem('totalScore')));
+});
+
+
+
+function drowMainPanel() {
+    mainPanel.style.left = String((window.innerWidth / 2) - (800 / 2)) + 'px';
+};
+
+
+//Работа с главным меню
+function closeMainPanel() {
+    mainPanel.style.display = 'none';
+};
+function showMainPanel() {
+    mainPanel.style.display = 'flex';
+};
+
+function onPlayGamePanel() {
+    playGamePanel.style.display = 'flex';
+    csorePanel.style.display = 'none';
+};
+function onCsorePanel() {
+    playGamePanel.style.display = 'none';
+    csorePanel.style.display = 'flex';
+};
+
+restartBtn.addEventListener('click', () => {
+    location.reload();
+});
+
+function closeCanvas() {
+    cvs.style.display = 'none';
+    // cvs.style.display = 'block';
+}
+
+function showCanvas() {
+    // cvs.style.display = 'none';
+    cvs.style.display = 'block';
+}
+
+drowMainPanel();
+window.addEventListener('resize', () => {
+    drowMainPanel();
+})
+
+
+
+
+function addScoreTable(date) {
+    if (!date) {
+        date = [];
+    };
+
+    let newTable = document.createElement('table');
+    newTable.className = 'tableScore';
+
+    function addDateToTable(UserName, score) {
+        let newTr = document.createElement('tr');
+        newTable.append(newTr);
+
+        let newTdUserName = document.createElement('td');
+        newTdUserName.className = 'tdTable';
+        newTdUserName.textContent = UserName;
+        newTr.append(newTdUserName);
+
+        let newTdScore = document.createElement('td');
+        newTdScore.className = 'tdTable';
+        newTdScore.textContent = score;
+        newTr.append(newTdScore);
+    };
+
+    addDateToTable('UserName', 'Score');
+
+    for (elem of date) {
+        addDateToTable(elem.userName, elem.score);
+    };
+    blockWithResults.append(newTable);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
